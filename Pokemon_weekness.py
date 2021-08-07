5f2c5a1c2f974f3b872028ea8300fb1b24fe4b5a
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-08-07 21:21:08 +0530

    tell the weekness types of pokemon by pokemon name

---
+++ Pokemon_weekness.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+Poke_name=input("Enter the name of pokemon: ")
+content=requests.get("https://www.pokemon.com/us/pokedex/"+Poke_name).text
+soup=BeautifulSoup(content,'lxml')
+
+try:
+      #getting data where with div and class=dtm-weeknesses
+      data=soup.find_all('div',{"class":"dtm-weaknesses"})
+      clean_data=data[0].text.split(' ')
+      weekness=[]
+      #print(clean_data)
+
+
+      #this are if for cleaning the unneccasary data
+      for i in clean_data:
+            if i!='' and i!='\n':
+                  weekness+=i
+      t=''
+      for i in weekness:
+            if i.isalpha():
+                  t+=i
+            else:
+                  if t!='':
+                        weekness.append(t)
+                        t=''
+
+
+      #print(weekness[::-1][:10])
+      #this will be the final weekness types
+      final_weekness=[] 
+      for i in weekness:
+            if len(i)>2 and i!='Weaknesses':
+                  final_weekness.append(i)
+      print(*final_weekness)
+
+
+except:
+      print("check the spelling of the pokemon")
